b5c6ccb09f4704e3cdb7f278a4ceb1c31147a978
A GÜL <asya0077@gmail.com>
2021-01-24 01:56:16 +0300

    düzenlendi

---
+++ OOP2/TuzelMusteri.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP2
+{
+    //Coorperate
+    //miras : inheritance
+    class TuzelMusteri : Musteri //Tüzel müşteri bir müşteridir
+    {
+        
+        public string SirketAdi { get; set; }
+        public string VergiNo { get; set; }
+    }
+}
