d3894dc1148f2290172d9aa28cb1fc7b1dc7acf6
A GÜL <asya0077@gmail.com>
2021-01-25 01:49:28 +0300

    Mydictionary

---
+++ Dictionarycalisma/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Dictionarycalisma
+{
+    class MyDictionary
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, int> AdNo = new Dictionary<string, int>();
+            AdNo.Add("Fatma", 123);
+            AdNo.Add("Sevgi", 456);
+            AdNo.Add("İlknur", 789);
+            AdNo.Add("Sonnur", 354);
+
+            foreach (var listele in AdNo)
+            {
+                Console.WriteLine(listele);
+            }
+            
+
+        }
+
+        
+    }
+}
